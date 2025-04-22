82c8603518fb01f82e151ace40a96d79901f659a
nicoleesanchez <nicole.valadii@hotmail.com>
2025-04-22 11:59:56 +0200

    forgot to push

--- ObjectWebApi/Models/IObjectProperties.cs
+++ ObjectWebApi/Models/IObjectProperties.cs
@@ -2,18 +2,9 @@
 namespace Models;
 public class ObjectProperties
 {
-<<<<<<< HEAD
     public Guid ObjectId { get; set; }
     public string Key { get; set; }
-=======
-    [Key]
-    public int Id { get; set; }
-
-    [ForeignKey("Object")]
-    public Guid ObjectID { get; set; }
-    public string Field { get; set; }
->>>>>>> 6c547b6ca49781ea59c81e794d82586dd3f9e247
     public string Value { get; set; }
 
-    public Object Object { get; set; }
+    public CustomObject Object { get; set; }
 }
