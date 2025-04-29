389035e826e6018cac953041e9413a64f9cc4799
nicoleesanchez <nicole.valadii@hotmail.com>
2025-04-29 14:34:02 +0200

    start design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +23,35 @@ function App() {
 }
 
 export default App;
+*/
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const handleImportCustomers = () => {
+    console.log('Import Customers clicked');
+    // Här kan du lägga till API-anrop etc.
+  };
+
+  const handleImportPersons = () => {
+    console.log('Import Persons clicked');
+    // Här kan du lägga till API-anrop etc.
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to our app</h1>
+        <p>Press the button to start</p>
+        <div className="button-group">
+        <button className="btn customer-btn" onClick={handleImportCustomers}>Customers</button>
+          <button className="btn person-btn" onClick={handleImportPersons}>Persons</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
