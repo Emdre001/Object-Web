589c2f835aeb7dfe954c4fa39bb58e446d2ce5ba
nicoleesanchez <nicole.valadii@hotmail.com>
2025-05-05 10:54:40 +0200

    knapparna brevid varandra

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -116,16 +116,19 @@ function App() {
 
         {error && <div className="error">{error}</div>}
         {/* Object type buttons (Person/Company) */}
-        {settingsLoaded && settings?.[0]?.applications?.[0]?.objectType?.map((objectType, idx) => (
-          <button
-            key={idx}
-            className="btn object-btn"
-            onClick={() => handleFetchObjects(objectType.name)}
-          >
-            {objectType.name}
-          </button>
-        ))}
-
+        {settingsLoaded && (
+        <div className="button-row">
+          {settings?.[0]?.applications?.[0]?.objectType?.map((objectType, idx) => (
+            <button
+              key={idx}
+              className="btn object-btn"
+              onClick={() => handleFetchObjects(objectType.name)}
+            >
+              {objectType.name}
+            </button>
+          ))}
+        </div>
+      )}
         {/* Display the list of objects based on the selected objectType */}
         {objects.length > 0 && (
           <div className="object-list">
