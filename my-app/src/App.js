dd4580482f582e104440f36d48917a634a517bc4
nicoleesanchez <nicole.valadii@hotmail.com>
2025-05-14 12:17:29 +0200

    List MUI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppLayout from "./AppLayout";
 import { HomePage } from './pages/Home';
 import { ListPage } from './pages/List';
+import { ListPage2 } from './pages/List2';
 import { ObjectPage } from './pages/Object';
 import { EditObjectPage } from './pages/EditObject';
 import { NotFound } from './pages/NotFound';
@@ -16,6 +17,7 @@ function App() {
         <Route path="/:appID/*" element={<AppLayout />}>
           <Route index element={<HomePage />} />
           <Route path="list/:objectType" element={<ListPage />} />
+          <Route path="list2/:objectType" element={<ListPage2 />} />
           <Route path="object/:objectID" element={<ObjectPage />} />
           <Route path="object/:objectID/edit" element={<EditObjectPage />} />
           <Route path="*" element={<NotFound />} />
